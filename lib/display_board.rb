ed52007b21186519447b2e8b02d35097bb6c07de
Robert W <robwash@gmail.com>
2018-01-14 20:13:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(board.new)
+def display_board(board = board.new)
 
    puts "   " "|" "   " "|" "   "
    puts "-----------"
